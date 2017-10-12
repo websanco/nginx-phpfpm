25a60bccf68715cd8d36e2118be8ce0cabeb5d69
websanco <32726791+websanco@users.noreply.github.com>
2017-10-13 00:52:06 +0700

    Create nginx-phpfpm.sh

---
+++ nginx-phpfpm.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This file is sourced by all *interactive* bash shells on startup,
+# including some apparently interactive shells such as scp and rcp
+# that can't tolerate any output.  So make sure this doesn't display
+# anything or bad things will happen !
+
+
+# Test for an interactive shell.  There is no need to set anything
+# past this point for scp and rcp, and it's important to refrain from
+# outputting anything in those cases.
+
+
+cd /usr/local/directadmin/custombuild
+./build update
+./build set php1_release 5.6
+./build set php2_release 5.4
+./build set php1_mode php-fpm
+./build set php2_mode php-fpm
+./build set mod_ruid2 no
+./build php n
+./build rewrite_confs
+cd /usr/local/directadmin/custombuild
+./build update
+./build clean
+./build suphp
+cd /usr/local/directadmin/custombuild
+./build set opcache yes
+./build opcache
+cd /usr/local/directadmin/custombuild
+./build update
+./build update_da
+./build set webserver nginx_apache
+./build nginx_apache
+./build rewrite_confs
+echo "action=rewrite&value=nginx" >> /usr/local/directadmin/data/task.queue  
+echo "action=rewrite&value=nginx" >> /usr/local/directadmin/dataskq
+service httpd restart
+service nginx restart
+cd /usr/local/directadmin
+echo "action=directadmin&value=restart" >> data/task.queue; ./dataskq d2000
+cd /usr/local/directadmin/custombuild
+./build roundcube
+./build squirrelmail
+./build phpmyadmin
+./build rewrite_confs
